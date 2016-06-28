a4655ab9ef430f693c59b915d8863b3717547cdb
Roman <thekreng@gmail.com>
2016-06-28 10:56:47 +0300

    Добавлена реализация класса WheatherData

--- SOURCE_CODE_/TestApp/TestConsoleApp/TestConsoleApp/CLASSES/OBSERVER/WeatherData.cs
+++ SOURCE_CODE_/TestApp/TestConsoleApp/TestConsoleApp/CLASSES/OBSERVER/WeatherData.cs
@@ -6,16 +6,160 @@ using System.Threading.Tasks;
 
 namespace TestConsoleApp.CLASSES
 {
-    class WeatherData
+    class WeatherData:Subject
     {
-        public WeatherData() {}
+        public WeatherData()
+        {
+            observers = new List<Observer>();
+        }
 
-        protected void getTemperature(){}
-        protected void getHimidity(){}
-        protected void getPressure(){}
+        public void registerObserver(Observer o)
+        {
+            observers.Add(o);
+        }
+        public void removeObserver(Observer o)
+        {
+            int i = observers.IndexOf(o);
+            if (i >= 0) observers.RemoveAt(i);
+        }
+        public void notifyObserver()
+        {
+            foreach (Observer o in observers)
+            {
+                o.update(temperature, humidity, pressure);
+            }
+        }
+
+        protected void setTemperature(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            measurementsChanged();
+        }
+
+        protected void getTemperature() { }
+        protected void getHimidity() { }
+        protected void getPressure() { }
+
+        public void measurementsChanged()
+        {
+            notifyObserver();
+        }
 
 
-        protected void measurementsChanged(){}
+        private List<Observer> observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+    }
 
+    public interface Observer
+    {
+       void update(float temperature, float humidity, float pressure);
+    }
+    public interface Subject
+    {
+        void registerObserver(Observer o);
+        void removeObserver(Observer o);
+        void notifyObserver();
+    }
+    public interface DisplayElement
+    {
+        void display();
+    }
+
+
+
+    public class CurrentConditionDisplay:Observer,DisplayElement
+    {
+        public CurrentConditionDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+        public void display()
+        {
+            Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+        }
+
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
+    }
+    public class StatisticDisplay : Observer, DisplayElement
+    {
+        public StatisticDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+        public void display()
+        {
+            Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+        }
+
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
+    }
+    public class ForecastDisplay : Observer, DisplayElement
+    {
+        public ForecastDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+        public void display()
+        {
+            Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+        }
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
+    }
+    public class ThirdPartyDisplay : Observer, DisplayElement
+    {
+        public ThirdPartyDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+        public void display()
+        {
+            Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+        }
+
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        private Subject weatherData;
     }
 }
